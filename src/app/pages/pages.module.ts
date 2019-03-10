8e00d01ad9407caf14eb0b9461c66df410287c8c
Teresa <teresagalaviza@gmail.com>
2019-03-10 12:33:00 -0700

    Graficas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,28 +6,39 @@ import { SharedModule } from "../shared/shared.module";
 //Rutas
 import { PAGES_ROUTES } from "./pages.routes";
 
+//ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from "./pages.component";
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
+import { InventarioComponent } from './inventario/inventario.component';
+import { PrestamosComponent } from './prestamos/prestamos.component';
+import { ActividadesComponent } from './actividades/actividades.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
+import { GraficoDonaComponent } from "../components/grafico-dona/grafico-dona.component";
 
 @NgModule({
     declarations:[
         PagesComponent,
         DashboardComponent,
-        ProgressComponent,
         Graficas1Component,
+        GraficoDonaComponent,
+        InventarioComponent,
+        PrestamosComponent,
+        ActividadesComponent,
+        UsuariosComponent,
     ],
     exports: [
         DashboardComponent,
-        ProgressComponent,
         Graficas1Component,
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        ChartsModule
     ]
 })
 export class PagesModule {}
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,17 +2,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const pagesRoutes: Routes = [
     { 
         path: '', 
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
+            { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+            { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuarios' } },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
     }
