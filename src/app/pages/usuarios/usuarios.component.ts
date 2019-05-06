889340ac69ff4eb3968e407f97811da26d50b4bd
Teresa <teresagalaviza@gmail.com>
2019-05-05 19:30:35 -0600

    Modulo Usuarios y errores

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+import { UsuarioService } from 'src/app/services/service.index';
+import { shallowEqualArrays } from '@angular/router/src/utils/collection';
 
+declare var swal: any;
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
@@ -7,9 +11,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+  usuarios: Usuario[] = [];
+  desde: number = 0;
+  totalRegistros: number = 0;
+
+  cargando: boolean =  true;
+
+  constructor(
+    public _usuarioService: UsuarioService
+  ) { }
 
   ngOnInit() {
+    this.cargarUsuarios();
   }
 
+  cargarUsuarios(){
+    this.cargando = true;
+
+    this._usuarioService.cargarUsuarios( this.desde )
+                        .subscribe( (resp: any) => {
+                          this.totalRegistros = resp.total;
+                          this.usuarios = resp.usuarios;
+
+                          if(this.desde >= this.totalRegistros){
+                            this.desde -= 5;
+                            this.cargarUsuarios();
+                          }
+
+                          this.cargando = false;
+                        });
+  }
+
+  cambiarDesde( valor: number ){
+    let desde = this.desde + valor;
+
+    if ( desde >= this.totalRegistros )
+      return;
+
+    if ( desde < 0 ) 
+      return;
+     
+    this.desde += valor;
+    this.cargarUsuarios();
+  }
+
+  buscarUsuario( termino: string ){
+
+    if ( termino.length <= 0) {
+      this.cargarUsuarios();
+      return;
+    }
+
+    //Se muestra el div que dice "Cargando"
+    this.cargando = true;
+
+    this._usuarioService.buscarUsuarios( termino )
+                          .subscribe( (usuarios: Usuario[]) => {
+                            
+                            this.usuarios = usuarios;
+                            this.cargando = false; //Se quita el div "Cargando"
+
+                          });
+
+  }
+
+  borrarUsuario( usuario: Usuario ){
+
+    if ( usuario._id === this._usuarioService.usuario._id ){
+      swal('No puede borrar usuario', 'No se puede borrar a si mismo', 'error');
+      return;
+    }
+
+    swal({
+      title: '¿Estas seguro?',
+      text: 'Estas a punto de borrar a ' + usuario.nombre,
+      icon: 'warning',
+      dangerMode: true,
+      buttons: true
+    }).then( borrar => {
+      if (borrar){
+        this._usuarioService.borrarUsuario( usuario._id )
+                          .subscribe( borrado => {
+                            console.log(borrado);
+                            
+                            this.cargarUsuarios();
+                          });
+      }
+    });
+
+
+  }
 }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 import { Usuario } from 'src/app/models/usuario.model';
 import { URL_SERVICIOS } from 'src/app/config/config';
-import {map} from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import swal from 'sweetalert';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -71,6 +73,10 @@ export class UsuarioService {
                       
                       return true;
 
+                    }),
+                    catchError( err => {
+                      swal('Error en el login', err.error.mensaje, 'error');
+                      return throwError( err );
                     })
                 );
                     
@@ -86,8 +92,41 @@ export class UsuarioService {
                       swal('Usuario creado', usuario.email, 'success');
                       return resp.usuario;
 
+                    }),
+                    catchError( err => {
+                      swal(err.error.mensaje, err.error.errors.message, 'error');
+                      return throwError( err );
                     })
                 );
                     
+  }
+
+  cargarUsuarios( desde: number = 0 ){
+    let url = URL_SERVICIOS + '/usuario?desde=' + desde;
+
+    return this.http.get( url );
+
+  }
+
+  buscarUsuarios( termino: string ){
+    let url = URL_SERVICIOS + '/busqueda/coleccion/usuarios/' + termino;
+
+    return this.http.get( url )
+                    .pipe( 
+                      map((resp: any) => resp.usuarios )
+                    );
+  }
+
+  borrarUsuario( id: string ){
+    let url = URL_SERVICIOS + '/usuario/' + id;
+    url += '?token=' + this.token;
+
+    return this.http.delete( url )
+                    .pipe( 
+                      map(resp => {
+                        swal('Usuario borrado', 'El usuario ha sido eliminado correctamente', 'success');
+                        return true;
+                      })
+                    );
   }
 }
